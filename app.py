182c02d080c0fa90a2fbf1563399205bd07052da
Matthew Gamble <git@matthewgamble.net>
2022-04-17 16:02:31 +1000

    Initial work on frontend interface
    This required storing a lot more metadata in the bucket.

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
+import dataclasses
 import os
+from datetime import datetime
 from http import HTTPStatus
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 from uuid import uuid4
+from zoneinfo import ZoneInfo
 
 from botocore.config import Config as BotoConfig
 from botocore.response import StreamingBody
-from flask import Flask, Response, json, request, stream_with_context
+from flask import Flask, Response, json, render_template, request, stream_with_context
 
+from cartifacts.util import istimestamp, s3_cp
 from cartifacts.vendor.flask_boto3 import Boto3
 
 if TYPE_CHECKING:
@@ -34,7 +38,34 @@ app.config["BOTO3_OPTIONAL_PARAMS"]["s3"]["kwargs"]["config"] = BotoConfig(
     },
 )
 
+
 boto = Boto3(app)
+
+
+APP_TZ = ZoneInfo(app.config["CARTIFACTS_TZ"])
+DT_FORMAT = app.config.get("CARTIFACTS_DT_FORMAT", "%Y-%m-%d %H:%M:%S")
+
+
+@dataclasses.dataclass(frozen=True, order=True, slots=True)
+class BuildId:
+    build_id: str = dataclasses.field(compare=False)
+    sortkey: str
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, BuildId):
+            return NotImplemented
+
+        return self.build_id == other.build_id and self.sortkey == other.sortkey
+
+    @property
+    def build_id_label(self) -> str:
+        return self.build_id.replace("$", "/")
+
+    @property
+    def created_at(self) -> str:
+        timestamp = int(self.sortkey)
+        dt = datetime.fromtimestamp(timestamp, tz=APP_TZ)
+        return dt.strftime(DT_FORMAT)
 
 
 def bad_request(msg: str) -> Response:
@@ -45,16 +76,87 @@ def bad_request(msg: str) -> Response:
     )
 
 
+@app.route("/", methods=["GET"])
+def home():
+    s3: S3Client = boto.clients.get("s3")
+
+    pipelines_prefix = "artifacts/pipelines/"
+    pipelines_response = s3.list_objects_v2(
+        Bucket=app.config["CARTIFACTS_BUCKET"],
+        Prefix=pipelines_prefix,
+        Delimiter="/",
+    )
+    pipeline_names: List[str] = list(s3_cp(pipelines_response))
+    while pipelines_response["IsTruncated"]:
+        pipelines_response = s3.list_objects_v2(
+            Bucket=app.config["CARTIFACTS_BUCKET"],
+            Prefix=pipelines_prefix,
+            Delimiter="/",
+            ContinuationToken=pipelines_response["NextContinuationToken"],
+        )
+        pipeline_names.extend(s3_cp(pipelines_response))
+
+    return render_template("home.html", pipeline_names=pipeline_names)
+
+
+@app.route("/pipeline/<pipeline>", methods=["GET"])
+def pipeline_view(pipeline: str):
+    s3: S3Client = boto.clients.get("s3")
+
+    metadata_response = s3.get_object(
+        Bucket=app.config["CARTIFACTS_BUCKET"],
+        Key=f"artifacts/pipelines/{pipeline}/metadata.json",
+    )
+    metadata = json.load(metadata_response["Body"])
+
+    builds_prefix = f"artifacts/pipelines/{pipeline}/builds/"
+    builds_response = s3.list_objects_v2(
+        Bucket=app.config["CARTIFACTS_BUCKET"],
+        Prefix=builds_prefix,
+        Delimiter="^",
+    )
+    from pprint import pprint
+    pprint(builds_response)
+    build_sortkeys: List[str] = list(s3_cp(builds_response))
+    while builds_response["IsTruncated"]:
+        builds_response = s3.list_objects_v2(
+            Bucket=app.config["CARTIFACTS_BUCKET"],
+            Prefix=builds_prefix,
+            Delimiter="^",
+            ContinuationToken=builds_response["NextContinuationToken"],
+        )
+        build_sortkeys.extend(s3_cp(builds_response))
+
+    def sep(build_sortkey: str) -> BuildId:
+        parts = build_sortkey.rpartition("$")
+        return BuildId(parts[0], parts[2][:-1])
+
+    build_ids_sorted = sorted(map(sep, build_sortkeys))
+
+    return render_template("pipeline/view.html", pipeline=pipeline, metadata=metadata, build_ids=build_ids_sorted)
+
+
+@app.route("/pipeline/<pipeline>/build/<build_id>", methods=["GET"])
+def build_view(pipeline: str, build_id: str):
+    pass
+
+
 @app.route("/api/upload", methods=["POST"])
-def upload():
+def api_upload():
     pipeline_header = request.headers.get("Cartifacts-Pipeline")
     build_id_header = request.headers.get("Cartifacts-Build-ID")
+    build_created_header = request.headers.get("Cartifacts-Build-Created")
     stage_id_header = request.headers.get("Cartifacts-Stage-ID")
     step_id_header = request.headers.get("Cartifacts-Step-ID")
     artifact_path_header = request.headers.get("Cartifacts-Artifact-Path")
     artifact_md5_header = request.headers.get("Cartifacts-Artifact-MD5")
+    repo_name_header = request.headers.get("Cartifacts-Repo-Name")
+    repo_link_header = request.headers.get("Cartifacts-Repo-Link")
 
-    all_headers = (pipeline_header, build_id_header, stage_id_header, step_id_header, artifact_path_header, artifact_md5_header)
+    all_headers = (
+        pipeline_header, build_id_header, build_created_header, stage_id_header, step_id_header,
+        artifact_path_header, artifact_md5_header, repo_name_header, repo_link_header,
+    )
 
     if any(map(lambda val: not bool(val), all_headers)):
         return bad_request("One or more metadata headers are missing.")
@@ -65,13 +167,15 @@ def upload():
     content_type = request.content_type
     if not content_type:
         return bad_request("Content Type header must be specified and non-empty.")
+    if not istimestamp(build_created_header):
+        return bad_request("Build Created header must be a plain UNIX timestamp.")
 
     pipeline = pipeline_header.replace("/", "$")
     build_id = build_id_header.replace("/", "$")
     stage_id = stage_id_header.replace("/", "$")
     step_id = step_id_header.replace("/", "$")
 
-    metadata = {
+    artifact_metadata = {
         "artifact_path": artifact_path_header,
         "content_length": content_length,
         "content_type": content_type,
@@ -80,25 +184,59 @@ def upload():
         "stage_id": stage_id_header,
         "step_id": step_id_header,
     }
+    build_metadata = {
+        "pipeline": pipeline_header,
+        "build_id": build_id_header,
+        "build_created": build_created_header,
+    }
+    pipeline_metadata = {
+        "pipeline": pipeline_header,
+        "repo_name": repo_name_header,
+        "repo_link": repo_link_header,
+    }
 
-    key_prefix = f"artifacts/pipelines/{pipeline}/builds/{build_id}/stages/{stage_id}/steps/{step_id}"
+    pipeline_key_prefix = f"artifacts/pipelines/{pipeline}"
+    builds_key_prefix = f"{pipeline_key_prefix}/builds"
+    build_key_prefix = f"{builds_key_prefix}/{build_id}"
+    artifact_key_prefix = f"{build_key_prefix}/stages/{stage_id}/steps/{step_id}"
     file_id = str(uuid4())
-    file_key = f"{key_prefix}/{file_id}"
-    metadata_key = f"{key_prefix}/{file_id}.json"
+    artifact_file_key = f"{artifact_key_prefix}/{file_id}"
+    artifact_metadata_key = f"{artifact_key_prefix}/{file_id}.json"
+    build_metadata_key = f"{build_key_prefix}/metadata.json"
+    builds_sort_key = f"{builds_key_prefix}/{build_id}${build_created_header}^"
+    pipeline_metadata_key = f"{pipeline_key_prefix}/metadata.json"
 
     s3: S3Client = boto.clients.get("s3")
 
     s3.put_object(
         Bucket=app.config["CARTIFACTS_BUCKET"],
-        Key=file_key,
+        Key=artifact_file_key,
         Body=StreamingBody(request.stream, content_length),
         ContentMD5=artifact_md5_header,
         ContentLength=content_length,
     )
     s3.put_object(
         Bucket=app.config["CARTIFACTS_BUCKET"],
-        Key=metadata_key,
-        Body=json.dumps(metadata),
+        Key=artifact_metadata_key,
+        Body=json.dumps(artifact_metadata),
+        ContentType="application/json",
+    )
+    s3.put_object(
+        Bucket=app.config["CARTIFACTS_BUCKET"],
+        Key=build_metadata_key,
+        Body=json.dumps(build_metadata),
+        ContentType="application.json",
+    )
+    s3.put_object(
+        Bucket=app.config["CARTIFACTS_BUCKET"],
+        Key=builds_sort_key,
+        Body=build_id_header,
+        ContentType="text/plain",
+    )
+    s3.put_object(
+        Bucket=app.config["CARTIFACTS_BUCKET"],
+        Key=pipeline_metadata_key,
+        Body=json.dumps(pipeline_metadata),
         ContentType="application/json",
     )
 
---
+++ cartifacts/util.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import os.path
+from typing import TYPE_CHECKING, Iterable
+
+if TYPE_CHECKING:
+    from mypy_boto3_s3.type_defs import ListObjectsV2OutputTypeDef
+
+
+def istimestamp(val: str) -> bool:
+    for char in val:
+        if char not in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
+            return False
+    return True
+
+
+def s3_cp(response: ListObjectsV2OutputTypeDef) -> Iterable[str]:
+    def clean(prefix: str) -> str:
+        return os.path.basename(prefix.rstrip("/"))
+
+    return map(lambda cp: clean(cp["Prefix"]), response["CommonPrefixes"])
+
+
+__all__ = ("istimestamp", "s3_cp")
